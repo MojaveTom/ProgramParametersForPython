9d093994c9a4bf7c139758e303f8cf63734a3c75
tom <tom@demayfamly.net>
2020-04-27 10:04:02 -0600

    Added function to read logging dictionarys. In one place, so I don't have to replicate in all my programs.

---
+++ progparams/GetLoggingDict.py
@@ -0,0 +1,82 @@
+
+'''
+A function to load a logging configuration dictionary from:
+    A keyword specifiec list of paths (paths=str or list)
+    -- or  --
+    A list of default configruation file locations.
+
+Parameters:
+    ProgName        required first argument: is the name of the program.
+    ProgPath        required second argument: is the path to the program.
+Keyword parameters:
+    paths           optional: is a file name or list of file names from
+                    which to attempt to load a configuration dictionary.
+The first file from the list that loads successfully is used; all
+others are ignored.
+File types: '.toml', '.jsonc', and '.json' are recognized and loaded.
+
+To define logging: the calling program needs to:
+
+from progparams.GetLoggingDict import GetLoggingDict
+config_dict = GetLoggingDict(ProgName, ProgPath)
+logging.config.dictConfig(config_dict)
+
+then create loggers like:
+
+errLogger = logging.getLogger("Debug  Logger")
+console = logging.getLogger("ConsoleLogger")
+'''
+
+import os               #   https://docs.python.org/3/library/os.html
+import toml             #   https://github.com/uiri/toml    https://github.com/toml-lang/toml
+# comment json strips python and "//" comments form json before applying json.load routines.
+import commentjson      #   https://github.com/vaidik/commentjson       https://commentjson.readthedocs.io/en/latest/
+# Lark is used by commentjson -- import commented out, but here for documentation.
+# import lark             #   https://github.com/lark-parser/lark    https://lark-parser.readthedocs.io/en/latest/
+
+def GetLoggingDict(ProgName: str, ProgPath: str, *args, **kwargs) -> dict :
+    ##############Logging Settings##############
+    #####  Setup logging; first try for file specific, and if it doesn't exist, use a folder setup file.
+    #
+    #   See https://docs.python.org/3/howto/logging-cookbook.html#formatting-styles for interesting ideas.
+    #
+    def updateLoggingDict(config_dict: dict) -> dict :
+        if 'log_file_path' in config_dict:
+            logPath = os.path.expandvars(config_dict['log_file_path'])
+            os.makedirs(logPath, exist_ok=True)
+        else:
+            logPath=""
+        for p in config_dict['handlers'].keys():
+            if 'filename' in config_dict['handlers'][p]:
+                fn = os.path.join(logPath, config_dict['handlers'][p]['filename'].replace('<replaceMe>', ProgName))
+                config_dict['handlers'][p]['filename'] = fn
+        return config_dict
+
+
+    if kwargs.get('paths') is not None:
+        paths = kwargs['paths']
+        if isinstance(paths, str): paths = list(paths)
+    else:
+        paths = (     os.path.join(ProgPath, ProgName + '_loggingconf.toml')        # in order to be checked
+                    , os.path.join(ProgPath, 'Loggingconf.toml')
+                    , os.path.join(ProgPath, ProgName + '_loggingconf.jsonc')
+                    , os.path.join(ProgPath, 'Loggingconf.jsonc')
+                    , os.path.join(ProgPath, ProgName + '_loggingconf.json')
+                    , os.path.join(ProgPath, 'Loggingconf.json')
+                )
+    for path in paths:
+        if not os.path.isfile(path): continue   # ignore paths entries that are not files.
+        name, ext = os.path.splitext(path)
+        try:
+            if ext == '.toml':
+                config_dict = toml.load(path)
+            elif (ext == '.json') or (ext == '.jsonc'):
+                config_dict = commentjson.load(open(path))
+            else:
+                continue
+            return updateLoggingDict(config_dict)
+        except Exception as e:
+            print(f"Attempt to read existing file: {path} failed.  Trying another file.")
+            pass
+    print("Logging configuration file not found.")
+    return {}
